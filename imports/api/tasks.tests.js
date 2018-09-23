389df008546e99225d8c6a0bb0744992bc8e8bf2
jackyk <jackykimani13@gmail.com>
2018-09-23 17:29:26 +0000

    adding Mocha- test did not work

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -5,6 +5,8 @@ import { Random } from 'meteor/random';
 
 import { Tasks } from './tasks.js';
 import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
@@ -23,6 +25,18 @@ if (Meteor.isServer) {
     });
 
       it('can delete owned task', () => {
+        // Find the internal implementation of the task method so we can
+      // test it in isolation
+      const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+      // Set up a fake method invocation that looks like what the method expects
+      const invocation = { userId };
+
+      // Run the method with `this` set to the fake invocation
+      deleteTask.apply(invocation, [taskId]);
+
+      // Verify that the method does what we expected
+      assert.equal(Tasks.find().count(), 0);
       });
     });
   });
